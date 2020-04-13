f23bb06341789be66d0661f018b4c5c86181dda9
Miguel Angel Rodríguez Chávez <rdz.miguel@live.com>
2020-04-13 10:40:28 -0500

    update navigate with HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+<div>
+  <h1>HATS PAGE</h1>
+</div>
+);
+
+const HatsDetailsPage = ({match}) => {
+  console.log(match);
+  return (
+  <div>
+    <h1>Hi {match.params.hatId}</h1>
+  </div>)
+};
+
 function App() {
   return (
-    <div>
-      <HomePage></HomePage>
-    </div>
+    <>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+        <Route path='/hats/:hatId' component={HatsDetailsPage}/>
+      </Switch>
+    </>
   );
 }
 
