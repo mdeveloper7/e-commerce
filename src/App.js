3ab1685206604fcda1792291581f2be2dbcabbd3
Miguel Angel Rodríguez Chávez <rdz.miguel@live.com>
2020-04-14 12:45:30 -0500

    add header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from "./components/header/header.component";
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -21,6 +22,7 @@ const HatsDetailsPage = ({match}) => {
 function App() {
   return (
     <>
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.styles.scss';
+import Logo from "../../assets/img/logo.jpg";
+
+const Header = () => (
+    <div className="header">
+        <Link className="logo-container" to="/">
+            <img alt="logo" src={Logo} className="logo"></img>
+        </Link>
+        <div className="options">
+            <Link className="option" to="/shop">
+                SHOP
+            </Link>
+            <Link className="option" to="/shop">
+                CONTACT
+            </Link>
+            <Link className="option" to="/shop">
+                SIGN IN
+            </Link>
+        </div>
+    </div>
+);
+
+export default Header;
