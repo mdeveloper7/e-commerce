4dc9715fb71635b6358b24655f26708fe556cde5
Miguel Rodriguez <c_mrodriguez@rev19.net>
2020-04-20 11:32:59 -0500

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/header/header.component";
 import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './pages/shop/shop.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -19,8 +19,9 @@ class App extends React.Component {
   unsuscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsuscribeFromAuth = auth.onAuthStateChanged( user=> {
-      this.setState({currentUser: user});
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(async user=> {
+      createUserProfileDocument(user);
+      // this.setState({currentUser: user});
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
     appId: "1:735960116450:web:16581745684898a5f382bb"
 }
 
+export const createUserProfileDocument = async (userAuth, addionalData)=>{
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapshot = await userRef.get();
+    
+    if(!snapshot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...addionalData
+            })
+        } catch(error){
+            console.log('error creating user', error.message);
+        }
+    }
+    
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
